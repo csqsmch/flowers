0648907fdf31a640f5733506371697bb6e4775f4
csqsmch <78016698+csqsmch@users.noreply.github.com>
2021-02-05 13:08:38 +0300

    lesson4

--- js/main.js
+++ js/main.js
@@ -1,25 +1,77 @@
-const flowerSlider = new Swiper('.flowers-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 6,
+$(document).ready(function () {
 
-  // Стрелки
-  navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
-  },
+  const flowerSlider = new Swiper('.flowers-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
 
-});
+    // Стрелки
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
+    //Настройки адаптивной версии
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 6,
+      }
+    }
+  });
 
-  // Стрелки
-  navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
-  },
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+
+    // Стрелки
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  });
+
+  $("#review-1").on('click', function () {
+
+    $.fancybox.open([
+      {
+        src: 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+        opts: {
+          caption: 'First caption',
+          thumb: 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+        }
+      },
+      {
+        src: 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+        opts: {
+          caption: 'Second caption',
+          thumb: 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+        }
+      },
+      {
+        src: 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+        opts: {
+          caption: 'First caption',
+          thumb: 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+        }
+      }
+    ], {
+      loop: true,
+      thumbs: {
+        autoStart: true
+      }
+    });
+
+  });
+
 
 });
